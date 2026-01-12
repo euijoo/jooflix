62a3fe6e808c3e7534516710b9422f492ae42ddc
euijoo <91236117+euijoo@users.noreply.github.com>
2026-01-13 03:32:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const heroSubtitle = document.getElementById("heroSubtitle");
 const heroDesc = document.getElementById("heroDesc");
 
 // 기본 히어로 소스 (원하면 바꿔도 됨)
-heroVideo.src = "https://www.w3schools.com/html/mov_bbb.mp4";
+heroVideo.src = "https://youtu.be/acUIyAfieHE?si=ZUug3bn3w9mvVYVj";
 
 
 
