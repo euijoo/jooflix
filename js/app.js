3932a49f38af25caae61d9464b84ab86791448d9
euijoo <91236117+euijoo@users.noreply.github.com>
2026-01-18 16:52:54 +0900

    Fix trailer URL escaping bug - change replace pattern from \\ to \\'

--- js/app.js
+++ js/app.js
@@ -203,8 +203,8 @@ function displayHeroSlide(movie) {
                 </div>
                 <div class="item-action" style="margin-top: 30px; display: flex; gap: 15px;">
                     ${movie.trailerUrl ? `
-    <button class="btn btn-hover" onclick="openVideoInModal('${movie.trailerUrl.replace(/'/g, "\\'")}')">
-        <i class='bx bx-play-circle'></i><span>예고편</span>
+                <button class="btn btn-hover" onclick="openVideoInModal('${movie.trailerUrl.replace(/'/g, "\\'")}')">
+                    <i class='bx bx-play-circle'></i><span>예고편</span>        <i class='bx bx-play-circle'></i><span>예고편</span>
     </button>
 ` : ''}
                     ${movie.externalVideoUrl ? `
@@ -306,6 +306,12 @@ function openVideoInModal(videoUrl) {
     let embedUrl = '';
     
     try {
+                // YouTube 비디오 ID만 들어온 경우 (11자리)
+                const youtubeIdPattern = /^[a-zA-Z0-9_-]{11}$/;
+                if (youtubeIdPattern.test(videoUrl)) {
+                                embedUrl = `https://www.youtube-nocookie.com/embed/${videoUrl}?autoplay=1&rel=0&modestbranding=1`;
+                                videoPlayer.src = embedUrl;
+                                videoModal.style.display = 'flex';
         // YouTube URL 파싱
         if (videoUrl.includes('youtube.com/watch?v=')) {
             const url = new URL(videoUrl);
